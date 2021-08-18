dcb1a24ac30b16f8f4755725e41a1d02e791792f
liufeihe <lfhe2014@163.com>
2021-08-18 11:41:15 +0800

    feat: pub/sub

--- src/service/redis.ts
+++ src/service/redis.ts
@@ -10,22 +10,26 @@ export class RedisService {
   private config;
   
   private redisService;
+  private redisSub;
+  private redisPub;
 	
   @Init()
   async init() {
     // console.log('redis', this.config)
   	this.redisService = new Redis(this.config);
-    // this.redisService.subscribe("mychannel", function (e) {
-    //   console.log('subscribe channel: mychannel');
-    // });
-    // this.redisService.on("message", function (channel, res) {
-    //   console.log('channel: ', channel);
-    //   console.log(typeof res); // string
-    //   console.log('res:', res);
-    // });
-    // this.redisService.on("error", function (err) {
-    //     console.log("response err:" + err)
-    // });
+  	this.redisPub = new Redis(this.config);
+  	this.redisSub = new Redis(this.config);
+    this.redisSub.subscribe("loggerChannel", function (e) {
+      console.log('subscribe channel: loggerChannel');
+    });
+    this.redisSub.on("message", function (channel, res) {
+      console.log('channel: ', channel);
+      console.log(typeof res); // string
+      console.log('res:', res);
+    });
+    this.redisSub.on("error", function (err) {
+        console.log("loggerChannel response err:" + err)
+    });
   }
   
   getRedisClient() {
@@ -34,6 +38,6 @@ export class RedisService {
 
   sendMsg(msg: string) {
     console.log('send msg', msg)
-    this.redisService.publish('mychannel', msg)
+    this.redisPub.publish('demoChannel', msg)
   }
 }
